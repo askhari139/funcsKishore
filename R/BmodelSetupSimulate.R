92476664facf2fd0ec0e3967914f97c946b6b2ab
askhari139 <askhari139@gmail.com>
2025-05-13 13:44:52 -0400

    update with Boolean package

--- R/BmodelSetupSimulate.R
+++ R/BmodelSetupSimulate.R
@@ -1,70 +1,37 @@
-BmodelSetup <- function(simPackage = "../Bmodel") {
-    wd <- getwd()
-    DirectoryNav(simPackage)
-    simPackage <- getwd()
-    download.file(
-        "https://github.com/askhari139/Boolean.jl/archive/refs/heads/main.zip",
-        destfile = "Bmodel.zip"
-    )
-    zipFile <- "Bmodel.zip"
-    unzip(zipFile, junkpaths = T)
-    file.remove(zipFile)
-    script <- readLines("script.jl")
-    script[1] <- paste0("include(\"", simPackage, "/bmodel.jl\")")
-    writeLines(script, "script.jl")
-
-    script <- readLines("scriptWindows.jl")
-    script[1] <- paste0("include(\"", simPackage, "/bmodel.jl\")")
-    writeLines(script, "scriptWindows.jl")
-    JuliaCall::julia_source("dependencyInstaller.jl")
-    setwd(wd)
+BmodelSetup <- function() {
+    x <- system("julia -e \"using Boolean\"", ignore.stderr = TRUE)
+    if (x == 1)
+        system("julia -e \" using Pkg; Pkg.add(url = \"https://github.com/askhari139/Boolean.jl\")\"")
 }
 
-simulation <- function(setupBmodel = F, simPackage = "../Bmodel", numThreads = 3) {
+simulation <- function(setupBmodel = F, simPackage = "../Bmodel", numThreads = 3, nInit = 100000) {
     nThreads <- parallel::detectCores()
     if(numThreads > 0.8*nThreads) {
         numThreads <- round(0.8*nThreads)
     }
     if (setupBmodel) {
-        if (!file.exists(paste0(simPackage, "/bmodel.jl")))
-            BmodelSetup(simPackage)
-    }
-    if (!dir.exists(simPackage)|| !file.exists(paste0(simPackage, "/bmodel.jl"))) {
-        message("Bmodel package not found. Run with setupBmodel = T if you would like to set it up")
-        return()
+        BmodelSetup()
     }
     wd <- getwd()
     setwd(simPackage)
     simPackage <- getwd()
     setwd(wd)
-    os <- .Platform$OS.type
-    script <- "script.jl"
-    if (os == "windows")
-        script <- "scriptWindows.jl"
-    file.copy(paste0(simPackage, "/", script), ".", overwrite = T)
-    if (os != "windows") {
-        Sys.setenv(JULIA_NUM_THREADS = as.character(numThreads))
-        command <- "julia script.jl"
+    topoFiles <- list.files(".", ".topo$")
+    size <- floor(length(topoFiles) / numThreads)
+    topoList <- lapply(1:numThreads, function(x) {
+        k <- (x - 1) * size
+        id <- 1:size + k
+        if (size + k > length(topoFiles))
+            id <- id[1]:(length(topoFiles))
+        topoFiles[id] %>% paste0(collapse = " ")
+    })
+    future::plan(future::multisession, workers = numThreads)
+    simulater <- future.apply::future_lapply(topoList, function(x) {
+        topoFiles <- paste0("[\"", paste0(x, collapse = "\",\""), "\"]")
+        command <- paste0("julia -e 'using Boolean; topoFiles = ", topoFiles, "; for topoFile in topoFiles; y = @elapsed x = bmodel_reps(topoFile; nInit = ",nInit,"); end'")
         system(command)
-    }
-    else {
-        topoFiles <- list.files(".", ".topo$")
-        size <- floor(length(topoFiles) / numThreads)
-        topoList <- lapply(1:numThreads, function(x) {
-            k <- (x - 1) * size
-            id <- 1:size + k
-            if (size + k > length(topoFiles))
-                id <- id[1]:(length(topoFiles))
-            topoFiles[id] %>% paste0(collapse = " ")
-        })
-        Sys.setenv(JULIA_NUM_THREADS = as.character(1))
-        future::plan(future::multisession, workers = numThreads)
-        simulater <- future.apply::future_lapply(topoList, function(x) {
-            command <- paste0("julia ", script, " ", x)
-            system(command)
-        })
-        future:::ClusterRegistry("stop")
-    }
+    })
+    future:::ClusterRegistry("stop")
 }
 
 simulateRACIPE <- function(racipePath, gkNorm = F, multiThread = T, numThreads = 8) {
